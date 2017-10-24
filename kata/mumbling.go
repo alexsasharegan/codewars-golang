409da3959392a39dce6a9f60887fd9abe67fcd6e
Alex Regan <alex.joseph.regan@gmail.com>
2017-10-23 19:25:18 -0700

    chore: fix comment

--- kata/mumbling.go
+++ kata/mumbling.go
@@ -5,12 +5,7 @@ import (
 	"unicode/utf8"
 )
 
-// Accum desc: This time no story, no theory. The examples below show you how to write function accum:
-// Examples:
-// accum("abcd") // -> "A-Bb-Ccc-Dddd"
-// accum("RqaEzty") // -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
-// accum("cwAt") // -> "C-Ww-Aaa-Tttt"
-// The parameter of accum is a string which includes only letters from a..z and A..Z.
+// Accum kata.
 func Accum(s string) string {
 	acc := make([]string, utf8.RuneCount([]byte(s)))
 	for i, r := range s {
